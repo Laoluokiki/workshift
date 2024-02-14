b8f1c6029627f2f62f243af2c8737273aca469c0
Olaoluwa Okikiade <devlaolu>
2024-02-15 00:04:26 +0100

    new url

--- api.py
+++ api.py
@@ -13,7 +13,8 @@ models.Base.metadata.create_all(bind=engine)
 
 origins = [
     "https://workshift-frontend-1ja6f0rtq-olaoluwas-projects-a9f0ed67.vercel.app",
-    "http://localhost:4200"
+    "http://localhost:4200",
+    "https://workshift-frontend.vercel.app"
     ]
 
 app.add_middleware(
